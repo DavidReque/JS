5cf964391664eac492494b3d8480d8d8bf2b18e2
DavidReque <108202767+DavidReque@users.noreply.github.com>
2022-11-19 19:08:07 -0600

    Add files via upload

---
+++ JS/1/sin.js
@@ -0,0 +1,14 @@
+const lista = [
+    "David",
+    18,
+    true,
+    new Date (2004, 5, 18),
+    libroFavorito = {
+        titulo:"Padre ico Padre pobre",
+        autor: "Robert Kiyosaki",
+        fecha: new Date(2012, 2, 2),
+        url: "https//www.amazon.com/Padre-rico-pobre-edici%C3%B3n-actualizada-ebook/dp/B007HPS120"
+    },
+];
+
+console.log(lista);
