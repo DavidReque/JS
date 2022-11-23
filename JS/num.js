96b3f638115fd5db7b339cc937fab3a1698b5dce
DavidReque <108202767+DavidReque@users.noreply.github.com>
2022-11-23 14:15:50 -0600

    Add files via upload

---
+++ JS/num.js
@@ -0,0 +1,17 @@
+let alturaInt = 170;
+let alturaFloat = 1.70;
+let pesoKg = 70.1;
+
+let altura = Math.ceil(alturaFloat);
+let peso = Math.floor(pesoKg);
+console.log(altura);
+console.log(peso);
+
+let iguales = Number.MAX_VALUE +1 === Number.MAX_VALUE;
+console.log(iguales);
+
+
+
+
+
+
