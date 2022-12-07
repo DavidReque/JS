80494dc40a015054191e6baa9646a359ad1eedbf
DavidReque <108202767+DavidReque@users.noreply.github.com>
2022-12-06 18:28:40 -0600

    Add files via upload

---
+++ JS/Set/Sets.js
@@ -0,0 +1,7 @@
+const miFamilia = new Set(["David", "Jorge", "Graciela", "Mariela", "Roberto"])
+miFamilia.add("David");
+
+console.log(miFamilia);
+
+miFamilia.add("JavaScript");
+console.log(miFamilia);
